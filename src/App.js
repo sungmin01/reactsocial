091d54f3f083edeace8a2e770cce97b803820c4d
sungmin <sungmin2340@naver.com>
2019-02-26 22:53:47 +0900

    top nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from "react";
 import Main from "./components/Main";
+import Toolbar from './components/Toolbar/Toolbar';
+import SideDrawer from './components/SideDrawer/SideDrawer';
+import Backdrop from './components/Backdrop/Backdrop';
 
 class App extends Component {
-  render() {
+          state = {
+           sideDrawerOpen: false
+         };
+          drawerToggleClickHandler = () => {
+          this.setState((prevState) => {
+          return {sideDrawerOpen: !prevState.sideDrawerOpen};
+          });
+        };
+
+        backdropClickHandler = () => {
+          this.setState({sideDrawerOpen: false});
+        };
+
+    render() {
+      let backdrop;
+
+      if (this.state.sideDrawerOpen) {
+        backdrop = <Backdrop click={this.backdropClickHandler} />
+    }
     return (
       <div>
-            <h1 className="title_web">React Social </h1>
+        <Toolbar drawerClickHandler={this.drawerToggleClickHandler} />
+        <SideDrawer show={this.state.sideDrawerOpen} />
+            {backdrop}
             <Main/>
       </div>
     );
